a0852149ebeeaf7f4f0e7a2d60bba3e84a556d29
AriDevK <AriadneFave@hotmail.com>
2023-12-28 15:02:08 -0500

    Added index route and log on files

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"io"
+	"os"
+)
 
 func main() {
 	r := gin.Default()
+
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello World! :)")
+	})
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // Sirve y escucha peticiones en 0.0.0.0:8080
+
+	r.Run()
 }
