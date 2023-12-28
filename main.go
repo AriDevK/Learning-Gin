197a4b112e2beb56d7711236eee11b6f4c57948d
AriDevK <AriadneFave@hotmail.com>
2023-12-28 15:07:21 -0500

    Managing cookies

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"io"
 	"os"
@@ -21,6 +22,17 @@ func main() {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
+	})
+
+	r.GET("/cookie", func(c *gin.Context) {
+		cookie, err := c.Cookie("gin_cookie")
+
+		if err != nil {
+			cookie = "NotSet"
+			c.SetCookie("gin_cookie", "test", 3600, "/", "localhost", false, true)
+		}
+
+		c.String(200, fmt.Sprintf("Cookie value: %s", cookie))
 	})
 
 	r.Run(":8080")
