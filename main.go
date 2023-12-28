f5ffb8246edf3adfa288ed0efe645e8b3c915460
AriDevK <AriadneFave@hotmail.com>
2023-12-28 15:21:49 -0500

    Change status code 200 to http.StatusOK

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"io"
+	"net/http"
 	"os"
 )
 
@@ -17,11 +18,11 @@ func main() {
 	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
 
 	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello World! :)")
+		c.String(http.StatusOK, "Hello World! :)")
 	})
 
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
@@ -34,11 +35,11 @@ func main() {
 			c.SetCookie("gin_cookie", "test", 3600, "/", "localhost", false, true)
 		}
 
-		c.String(200, fmt.Sprintf("Cookie value: %s", cookie))
+		c.String(http.StatusOK, fmt.Sprintf("Cookie value: %s", cookie))
 	})
 
 	r.GET("/form", func(c *gin.Context) {
-		c.HTML(200, "form.tmpl", gin.H{
+		c.HTML(http.StatusOK, "form.tmpl", gin.H{
 			"title": "Main website",
 		})
 	})
@@ -47,7 +48,7 @@ func main() {
 		message := c.PostForm("message")
 		nick := c.DefaultPostForm("nick", "anonymous")
 
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"status":  "posted",
 			"message": message,
 			"nick":    nick,
